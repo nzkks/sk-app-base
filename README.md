654749b1e502483b86d2468ae64a5915ad40a793
ShanthoshK <kkshanthosh@gmail.com>
2016-08-29 21:31:40 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# sk-app-base
+[Author](http://www.drmsweb.com/)
+
+Use this base structure to start any website or web application
+
+This has following Gulp workflows ready
+- lint, concatinate and minify the custom js files
+- concatinate and copy the vendor js files
+- convert the sass file(s) and minify the css file
+- copy the html
+- watch the changes in html, sass files and js files and does above tasks
+
+Install
+- Just download or clone the repository
+- In the copied folder run these commands from command line interface
+..* npm init
+..* npm install
+..* gulp
+- The last line will update the files in the dist folder. If you don't have the folder it creates and copy the files. Simultaneously a browser window will start to open the index.html file from the dist folder.
+
+
+*TODO*
+- add PUG (Jade templating) support to HTML
+..* obviously adding that workflow in the gulp
---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var gulp                = require('gulp'),
+    jsHint              = require('gulp-jshint'),
+    concat              = require('gulp-concat'),
+    uglify              = require('gulp-uglify'),
+    sourceMaps          = require('gulp-sourcemaps'),
+    copy                = require('gulp-contrib-copy'),
+    plumber             = require('gulp-plumber'),
+    sass                = require('gulp-sass'),
+    cleanCss            = require('gulp-clean-css'),
+    autoPrefixer        = require('gulp-autoprefixer'),
+    rename              = require('gulp-rename'),
+    runSequence         = require('run-sequence'),
+    browserSync         = require('browser-sync').create(),
+    reload              = browserSync.reload;
+
+var jsSRC               = './src/js/*.js',
+    jsDEST              = 'dist/',
+    concatJsFile        = 'main.min.js',
+    vendorsJsSRC        = './src/js/vendors/**/*.js',
+    concatVendorJsFile  = 'vendors.min.js',
+    sassSRC             = './src/sass',
+    cssDEST             = 'dist/',
+    autoPrefixBrowsers  = ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
+    htmlSRC             = './src/index.html',
+    htmlDEST            = 'dist/';
+
+gulp.task('cssifySass', function(){
+  return gulp.src(sassSRC +'/main.sass')
+      .pipe(sourceMaps.init())
+      .pipe(sass.sync().on('error', sass.logError))
+      .pipe(autoPrefixer({browsers: autoPrefixBrowsers}))
+      .pipe(cleanCss())
+      .pipe(rename({extname: ".min.css"}))
+      .pipe(sourceMaps.write())
+      .pipe(gulp.dest(cssDEST));
+});
+
+gulp.task('copyHTML', function(){
+  return gulp.src(htmlSRC)
+      .pipe(plumber())
+      .pipe(copy())
+      .pipe(gulp.dest(htmlDEST));
+});
+
+gulp.task('copyVendorsScripts', function(){
+  return gulp.src(vendorsJsSRC)
+      .pipe(plumber())
+      .pipe(concat(concatVendorJsFile))
+      .pipe(gulp.dest(jsDEST));
+});
+
+gulp.task('lint', function(){
+  return gulp.src(jsSRC)
+      .pipe(plumber())
+      .pipe(jsHint())
+      .pipe(jsHint.reporter('default'));
+});
+
+gulp.task('concatAndMinifyScripts', function(){
+  return gulp.src(jsSRC)
+      .pipe(sourceMaps.init())
+      .pipe(plumber())
+      .pipe(concat(concatJsFile))
+      .pipe(uglify())
+      .pipe(sourceMaps.write())
+      .pipe(gulp.dest(jsDEST));
+});
+
+gulp.task('serve', function(){
+  browserSync.init({
+    server: './dist/'
+  });
+
+  gulp.watch('./src/sass/**/*.sass', ['cssifySass']).on('change', reload);
+  gulp.watch(jsSRC, ['lint', 'concatAndMinifyScripts']).on('change', reload);
+  gulp.watch(vendorsJsSRC, ['copyVendorsScripts']).on('change', reload);
+  gulp.watch(htmlSRC, ['copyHTML']).on('change', reload);
+});
+
+gulp.task('default', function(callback){
+  runSequence(['serve', 'cssifySass', 'copyHTML', 'copyVendorsScripts', 'lint', 'concatAndMinifyScripts'], callback);
+});
